58f2bad3aefd5a24d4ffc78cebc9c0bfa58650a6
sanchitasakhare912-dotcom <sanchitasakhare912@gmail.com>
2026-02-17 00:30:47 +0530

    Update caregiver.js

---
+++ caregiver.js
@@ -0,0 +1,9 @@
+function goMedicine() {
+   window.location.href = "set-medicine.html";
+}
+function goRoutine() {
+    window.location.href = "edit-routine.html";
+}
+function goPeople() {
+    window.location.href = "people.html";
+}
